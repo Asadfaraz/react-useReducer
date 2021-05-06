dbcaf3d54d420bc9fb59364b396647de2c6d63e9
Asad Faraz <asadfaraz44@gmail.com>
2021-05-06 15:13:08 +0500

    functionality added with styles

--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -1,12 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Model from "./Model";
 import { data } from "../api";
 
 const UseReducer = () => {
+  const [name, setName] = useState("");
+  const [people, setPeople] = useState(data);
+  const [showModel, setShowModel] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (name) {
+      setShowModel(true);
+      setPeople([...people, { id: new Date().getTime().toString, name }]);
+
+      setName("");
+    } else {
+      setShowModel(true);
+    }
+  };
+
   return (
-    <div>
-      <h1>UseReducer</h1>
-    </div>
+    <>
+      {showModel && <Model />}
+
+      <form onSubmit={handleSubmit} className="form">
+        <div>
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => {
+              setName(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit">add</button>
+      </form>
+
+      {/* show list */}
+      {people.map((person) => {
+        const { id, name } = person;
+        return (
+          <div key={id}>
+            <h4>{name}</h4>
+          </div>
+        );
+      })}
+    </>
   );
 };
 
