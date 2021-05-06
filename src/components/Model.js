754ce719e8938ae00717f51079d6d60b33946875
Asad Faraz <asadfaraz44@gmail.com>
2021-05-06 14:55:11 +0500

    data added

---
+++ src/components/Model.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Model = () => {
+  return <div>i'm model</div>;
+};
+
+export default Model;
--- src/components/UseReducer.js
+++ src/components/UseReducer.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Model from "./Model";
+import { data } from "../api";
 
 const UseReducer = () => {
   return (
